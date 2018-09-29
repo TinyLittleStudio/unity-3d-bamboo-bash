fa7aa00373e4e452d28e2499beaa19929fe37243
Onur Alici <onur.alici@live.de>
2018-09-29 21:09:10 +0200

    Auto stash before merge of "development" and "origin/development"

---
+++ Bamboo Bash/Assets/Scripts/Character/Profile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Profile {
+    [SerializeField] private string name;
+    [SerializeField] private GameObject prefab;
+
+    public Profile(string name, GameObject prefab)
+    {
+        this.name = name;
+        this.prefab = prefab;
+    }
+
+    public string Name => name;
+
+    public GameObject Prefab => prefab;
+
+    public override string ToString()
+    {
+        return name;
+    }
+}
