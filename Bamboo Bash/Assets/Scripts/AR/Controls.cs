3d3ca6b3032a2ae3d59fdb8dab3671613a23a214
Onur Alici <onur.alici@live.de>
2018-09-29 19:02:25 +0200

    UI: Base Screens
    - Added End Screen
    - Added Start Screen

--- Bamboo Bash/Assets/Scripts/AR/Controls.cs
+++ Bamboo Bash/Assets/Scripts/AR/Controls.cs
@@ -1,17 +1,8 @@
 ﻿using UnityEngine;
-using TMPro;
 
 public class Controls : MonoBehaviour
 {
-    [SerializeField]
-    private TextMeshProUGUI text;
-
-    private bool gyroEnabled;
-    private Gyroscope gyro;
-
     public GameObject obj;
-
-    private Quaternion rotation;
 
     private Transform cameraContainer;
 
@@ -20,49 +11,18 @@ public class Controls : MonoBehaviour
         cameraContainer = Camera.main.transform;
         transform.SetParent(cameraContainer);
         transform.rotation = cameraContainer.rotation;
-        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
-
-        gyroEnabled = EnableGyro(); 
-    }
-
-    private bool EnableGyro()
-    {
-        /*if (SystemInfo.supportsGyroscope)
-        {
-            gyro = Input.gyro;
-            gyro.enabled = true;
-
-            cameraContainer.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-            rotation = new Quaternion(0, 0, 1, 0);
-
-            return true;
-        }
-        */
-        return false;
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane)); 
     }
 
     private void Update()
     {
-        if (gyroEnabled)
-        {
-            transform.localRotation = gyro.attitude * rotation;
-        }
-
         RaycastHit hit;
 
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            text.text =Camera.main.transform.rotation + ", " + hit.transform.name + ", " + hit.point;
-
             if(obj != null && hit.transform.tag != "No Raycast Target") {
                 obj.transform.position = hit.point;
             }
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            text.text = "none";
         }
     }
 }
