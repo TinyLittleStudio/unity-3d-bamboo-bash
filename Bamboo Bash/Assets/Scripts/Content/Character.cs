cf793d58f9b72b3a5b0b54e99c9391bade2ab8a8
Onur Alici <onur.alici@live.de>
2018-09-30 12:19:45 +0200

    Final

--- Bamboo Bash/Assets/Scripts/Content/Character.cs
+++ Bamboo Bash/Assets/Scripts/Content/Character.cs
@@ -6,18 +6,15 @@ namespace TinyLittleStudio.BambooBash.Content
 {
     public class Character : MonoBehaviour
     {
-        private static readonly float HEALTH_BAR_LENGTH = 3.0f;
-
         [Header("Settings")]
         [SerializeField] private GameObject target;
         [SerializeField] private GameObject modelTarget;
-        [SerializeField] private GameObject healthBar;
 
         private bool isInitialized = false;
 
         private void Awake()
         {
-            if (target == null || modelTarget == null || healthBar == null)
+            if (target == null || modelTarget == null)
             {
                 throw new Exception("Missing Elements On Character-Script!");
             }
@@ -27,49 +24,34 @@ namespace TinyLittleStudio.BambooBash.Content
         {
             if (TouchUtils.Gestures.IsLeft)
             {
-                Spawn(PrimitiveType.Cube);
+                GameObject gameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                gameObject.AddComponent<Rigidbody>();
+                gameObject.AddComponent<BoxCollider>();
+
+                gameObject.transform.SetParent(Manager.DefaultInstance.GeneralScene.transform);
+                gameObject.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 6.0f, target.transform.position.z);
+                gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+                gameObject.GetComponent<Renderer>().material.color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
             }
 
             if (TouchUtils.Gestures.IsRight)
             {
-                Spawn(PrimitiveType.Sphere);
-            }
+                GameObject gameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                gameObject.AddComponent<Rigidbody>();
+                gameObject.AddComponent<SphereCollider>();
 
-            if (TouchUtils.Gestures.IsDoubleTap)
-            {
-                Notification.Notify("Du hast dir selbst Schaden zugefügt?", Notification.Level.WARNING);
-                Damage(10.0f);
+                gameObject.transform.SetParent(Manager.DefaultInstance.GeneralScene.transform);
+                gameObject.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 6.0f, target.transform.position.z);
+                gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+                gameObject.GetComponent<Renderer>().material.color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
             }
 
             if (TouchUtils.Gestures.IsDown)
             {
-                Notification.Notify("Du bist gestorben!", Notification.Level.ERROR);
-                Die();
+                Manager.DefaultInstance.Reset();
             }
-
-            CurrentHealth = Mathf.Clamp(CurrentHealth, MinHealth, MaxHealth);
-
-            if (healthBar != null)
-            {
-                float length = (CurrentHealth / MaxHealth) * 100.0f * (Character.HEALTH_BAR_LENGTH / 100.0f);
-
-                healthBar.transform.localScale = new Vector3(length, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
-                healthBar.transform.LookAt(Camera.main.transform.position);
-            }
-        }
-
-        private GameObject Spawn(PrimitiveType primitiveType)
-        {
-            GameObject gameObject = GameObject.CreatePrimitive(primitiveType);
-            gameObject.AddComponent<Rigidbody>();
-            gameObject.AddComponent<BoxCollider>();
-
-            gameObject.transform.SetParent(Manager.DefaultInstance.GeneralScene.transform);
-            gameObject.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 10.0f, target.transform.position.z);
-            gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-
-            gameObject.GetComponent<Renderer>().material.color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-            return gameObject;
         }
 
         public void OnProfileChange()
@@ -79,36 +61,8 @@ namespace TinyLittleStudio.BambooBash.Content
                 GameObject model = Instantiate(Profile.Prefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
                 model.transform.SetParent(modelTarget.transform);
                 model.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-
-                CurrentHealth = Profile.Health;
             }
         }
-
-        public void Damage(float damage)
-        {
-            CurrentHealth -= damage;
-
-            if (CurrentHealth <= 0)
-            {
-                Die();
-            }
-        }
-
-        public void Die()
-        {
-            Manager.DefaultInstance.EndScreen.SetActive(true);
-
-            if (Manager.DefaultInstance.GeneralScene != null)
-            {
-                Destroy(Manager.DefaultInstance.GeneralScene);
-            }
-        }
-
-        public float CurrentHealth { get; set; }
-
-        public float MinHealth => 0;
-
-        public float MaxHealth => Profile.Health;
 
         public Profile Profile { get; set; }
     }
--- Bamboo Bash/Assets/Scripts/Content/Profile.cs
+++ Bamboo Bash/Assets/Scripts/Content/Profile.cs
@@ -4,28 +4,17 @@ using UnityEngine;
 [Serializable]
 public class Profile
 {
-    public static readonly float DEFAULT_HEALTH = 10.0f;
-
     [Header("Settings")]
     [SerializeField] private string name;
     [SerializeField] private string description;
 
-    [SerializeField] private float health;
-
     [Header("Misc")]
     [SerializeField] private GameObject prefab;
-
-    public Profile(string name, string description, GameObject prefab) : this(name, description, Profile.DEFAULT_HEALTH, prefab)
-    {
-
-    }
 
     public Profile(string name, string description, float health, GameObject prefab)
     {
         this.name = name;
         this.description = description;
-
-        this.health = health;
 
         this.prefab = prefab;
     }
@@ -33,8 +22,6 @@ public class Profile
     public string Name => name;
 
     public string Description => description;
-
-    public float Health => health;
 
     public GameObject Prefab => prefab;
 
